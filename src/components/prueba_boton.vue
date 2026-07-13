e8908d8cc551ef2ab22ac1b991101434b99ded41
j0clr0 <clarosrocajosue@gmail.com>
2026-07-13 19:22:24 -0400

    feature: agregnad boton como componente reutilizable

---
+++ src/components/prueba_boton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button 
+    :class="[
+      'btn-base',
+      `btn-${variante}`,
+      { 'btn-pequeno': tamano === 'pequeno' },
+      { 'btn-grande': tamano === 'grande' },
+      { 'btn-bloque': bloque }
+    ]"
+    :disabled="deshabilitado"
+    @click="manejarClick"
+  >
+    <span v-if="icono" class="btn-icono">{{ icono }}</span>
+    <span class="btn-texto">{{ texto }}</span>
+  </button>
+</template>
