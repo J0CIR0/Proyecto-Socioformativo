c31a2c5a98cd3d939fadc036ebb3f33e7167a894
MeliStarr <desingdaza90@gmail.com>
2026-07-13 22:36:16 -0400

    Agregado componente reutilizable TarjetaPersonalizada

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <script setup>
-import BaseBoton from './components/prueba_boton.vue'
+    import BaseBoton from './components/prueba_boton.vue'
+    //
+    import BotonPersonalizado from './components/BotonPersonalizado.vue'
 </script>
 
 <template>
-  <main>
-    <h1>Sistema de Gestión Emprendimientos</h1>
-    <p>Proyecto de Programación Web II</p>
-    
-    <div style="margin-top: 30px; display: flex; gap: 12px; flex-wrap: wrap;">
-      <BaseBoton texto="Primario" @click="console.log('Click')" />
-      <BaseBoton texto="Éxito" variante="exito" />
-      <BaseBoton texto="Peligro" variante="peligro" />
-      <BaseBoton texto="Pequeño" tamano="pequeno" variante="info" />
-      <BaseBoton texto="Deshab." :deshabilitado="true" variante="oscuro" />
-    </div>
-  </main>
+    <main>
+        <h1>Sistema de Gestión Emprendimientos</h1>
+        <p>Proyecto de Programación Web II</p>
+
+        <div style="margin-top: 30px; display: flex; gap: 12px; flex-wrap: wrap;">
+            <BaseBoton texto="Primario" @click="console.log('Click')" />
+            <BaseBoton texto="Éxito" variante="exito" />
+            <BaseBoton texto="Peligro" variante="peligro" />
+            <BaseBoton texto="Pequeño" tamano="pequeno" variante="info" />
+            <BaseBoton texto="Deshab." :deshabilitado="true" variante="oscuro" />
+        </div>
+        <!--tarj mel-->
+        <h2>Mi componente reutilizable</h2>
+
+        <BotonPersonalizado
+         titulo="Cafetería"
+         descripcion="Este es un componente reutilizable creado para el proyecto." />
+    </main>
 </template>
 
+
 <style scoped>
-main {
-  max-width: 900px;
-  margin: 40px auto;
-  font-family: Arial, sans-serif;
-}
-h1 {
-  margin-bottom: 8px;
-}
+    main {
+        max-width: 900px;
+        margin: 40px auto;
+        font-family: Arial, sans-serif;
+    }
+
+    h1 {
+        margin-bottom: 8px;
+    }
 </style>
---
+++ src/components/BotonPersonalizado.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="tarjeta">
+        <h3>{{ titulo }}</h3>
+        <p>{{ descripcion }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+  titulo: {
+    type: String,
+    default: 'Título
+  },
+  descripcion: {
+    type: String,
+    default: 'Descripción'
+  }
+})
+</script>
+
+<style scoped>
+    .tarjeta {
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        padding: 20px;
+        width: 250px;
+        box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+    }
+    
+    h3 {
+        margin-bottom: 10px;
+    }
+</style>
